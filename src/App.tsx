b01cb5e20f3a468eec8598de426739999a2bc746
Mael RIOUX <mael.rioux@axopen.com>
2022-12-21 21:35:29 +0100

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,13 @@
-import './App.css'
-import { Test } from "./pages/Test/index"
-import './i18n'
-import { useTranslation } from 'react-i18next'
-import { useEffect } from 'react'
-
-
+import "./App.css";
+import { Test } from "./pages/Test/index";
+import "./i18n";
 
 function App() {
-
-  const languages = [
-    {
-
-      code: 'fr',
-      name: 'Français',
-      country_code: 'fr',
-    },
-    {
-      code: 'en',
-      name: 'English',
-      country_code: 'gb',
-    },
-  ]
-
-  const currentLanguageCode = localStorage.getItem('i18nextLng') || 'fr'
-  const currentLanguage = languages.find((l) => l.code === currentLanguageCode)
-  const { t } = useTranslation()
-
-
-  useEffect(() => {
-    console.log('Setting page stuff')
-    document.title = t('app_title')
-  }, [currentLanguage, t])
-
-
   return (
     <>
-    <Test/>
+      <Test />
     </>
   );
 }
 
-export default App
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,14 @@
-import React, { Suspense } from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './i18n'
+import React, { Suspense } from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./i18n";
 
+const loadingMarkup = <div>Loading translation ...</div>;
 
-const loadingMarkup = (
-  <div>Loading translation ...</div>
-)
-
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <Suspense fallback={loadingMarkup}>
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-  </Suspense>,
-)
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Suspense>
+);
